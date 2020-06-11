e145aa66c624cb6aca944d5d4652c660d78420d9
Spencer Du <spencer.du@embl.de>
2020-06-11 03:03:43 +0200

    create and update documents route

--- routes/api/documents.js
+++ routes/api/documents.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator');
 
 const Documents = require('../../models/Documents');
 const User = require('../../models/User');
@@ -20,6 +21,34 @@ router.get('/me', auth, async (req, res) => {
         .json({ msg: 'There is no bibtex database document(s) for this user' });
     }
 
+    res.json(documents);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route   POST api/documents
+// @desc    Create or update user document(s)
+// @access  Private
+router.post('/', [auth], async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  const {} = req.body;
+
+  const documentsFields = {
+    user: req.user.id,
+  };
+
+  try {
+    // Using upsert option (creates new doc if no match is found):
+    let documents = await Documents.findOneAndUpdate(
+      { user: req.user.id },
+      { $set: documentsFields },
+      { new: true, upsert: true }
+    );
     res.json(documents);
   } catch (err) {
     console.error(err.message);
