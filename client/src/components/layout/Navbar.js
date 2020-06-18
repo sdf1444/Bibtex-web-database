56f0dddfc68edaaf96dcef48300deee98bed365b
Spencer Du <spencer.du@embl.de>
2020-06-18 15:28:27 +0200

    logout & navbar links

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,18 +1,40 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { PropTypes } from 'prop-types';
+import { logout } from '../../actions/auth';
 
-const Navbar = () => {
+const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
+  const authLinks = (
+    <ul>
+      <li>
+        <a onClick={logout} href='#!'>
+          <i className='fas fa-sign-out-alt'></i>{' '}
+          <span className='hide-sm'></span>Logout
+        </a>
+      </li>
+    </ul>
+  );
+
   return (
-    <div>
-      <nav className='navbar bg-dark'>
-        <h1>
-          <Link to='/'>
-            <i className='fas fa-code' /> Bibtex
-          </Link>
-        </h1>
-      </nav>
-    </div>
+    <nav className='navbar bg-dark'>
+      <h1>
+        <Link to='/'>
+          <i className='fas fa-code' /> Bibtex
+        </Link>
+      </h1>
+      {!loading && <Fragment>{isAuthenticated ? authLinks : null}</Fragment>}
+    </nav>
   );
 };
 
-export default Navbar;
+Navbar.propTypes = {
+  logout: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+});
+
+export default connect(mapStateToProps, { logout })(Navbar);
