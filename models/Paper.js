80bf2ceba6752b326e98b1b13d02e9d7d5415282
Spencer Du <spencer.du@embl.de>
2020-06-22 01:33:12 +0200

    paper modelschema

---
+++ models/Paper.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PaperSchema = new mongoose.Schema({
+  paper: {
+    type: String,
+  },
+  doi: {
+    type: String,
+  },
+  pdf: {
+    type: String,
+  },
+});
+
+module.exports = Paper = mongoose.model('paper', PaperSchema);
