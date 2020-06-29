d09b55cad514949a407f58950067fd77a0a4ec38
Spencer Du <spencer.du@embl.de>
2020-06-29 19:04:13 +0200

    'update'

--- client/src/App.js
+++ client/src/App.js
@@ -5,16 +5,9 @@ import Home from './components/layout/Homepage';
 import Login from './components/auth/Login';
 import Admin from './components/admin/Admin';
 import CreateUser from './components/admin/CreateUsers';
-<<<<<<< HEAD
 import Alert from './components/layout/Alert';
 import Editor from './components/editor/Editor';
 import Paper from './components/paper/Paper';
-=======
-import EditUser from './components/admin/EditUsers';
-import Editor from './components/editor/Editor';
-import Paper from './components/paper/Paper';
-import Alert from './components/layout/Alert';
->>>>>>> fd6a7f0bc79d716d607390723967b6cd654abb4e
 import PrivateRoute from './components/routing/PrivateRoute';
 // Redux
 import { Provider } from 'react-redux';
@@ -43,18 +36,10 @@ const App = () => {
             <Alert />
             <Switch>
               <Route exact path='/login' component={Login} />
-<<<<<<< HEAD
               <PrivateRoute exact path='/admin' component={Admin} />
               <PrivateRoute exact path='/create-users' component={CreateUser} />
               <PrivateRoute exact path='/editor' component={Editor} />
               <PrivateRoute exact path='/papers' component={Paper} />
-=======
-              <Route exact path='/admin' component={Admin} />
-              <Route exact path='/create-users' component={CreateUser} />
-              <PrivateRoute exact path='/editor' component={Editor} />
-              <Route exact path='/edit-users/:id' component={EditUser} />
-              <Route exact path='/papers' component={Paper} />
->>>>>>> fd6a7f0bc79d716d607390723967b6cd654abb4e
             </Switch>
           </section>
         </Fragment>
