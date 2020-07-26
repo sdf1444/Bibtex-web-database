25d91e4cb5529f3e473bcaa7bb12adab4fb9a8d4
Spencer Du <spencer.du@embl.de>
2020-07-26 22:03:19 +0200

    re deploy

--- client/src/actions/bibtex.js
+++ client/src/actions/bibtex.js
@@ -1,29 +1,29 @@
 const convertMiddleLine = (key, value) => {
-    return `${key} = "${value}",\n`
-}
+  return `${key} = "${value}",\n`;
+};
 
 const convertLastLine = (key, value) => {
-    return `${key} = "${value}"}\n\n`
-}
+  return `${key} = "${value}"}\n\n`;
+};
 
 const convertRef = (ref) => {
-    let type = ref.entryType.toLowerCase();
-    const entries = Object.entries(ref.entryTags)
-    const strings = entries.map(([key, value], i) => {
-        if (i === entries.length - 1) {
-            return convertLastLine(key, value);
-        }
-        return convertMiddleLine(key, value);
-    })
-    let finalString = `@${type}{${ref.citationKey},\n`;
-    strings.forEach(str => finalString += str);
-    return finalString;
-}
+  let type = ref.entryType.toLowerCase();
+  const entries = Object.entries(ref.entryTags);
+  const strings = entries.map(([key, value], i) => {
+    if (i === entries.length - 1) {
+      return convertLastLine(key, value);
+    }
+    return convertMiddleLine(key, value);
+  });
+  let finalString = `@${type}{${ref.citationKey},\n`;
+  strings.forEach((str) => (finalString += str));
+  return finalString;
+};
 
 export const convertRefs = (refs) => {
-    let finalString = "";
-    refs.forEach(ref => {
-        finalString += convertRef(ref);
-    })
-    return finalString;
-}
+  let finalString = '';
+  refs.forEach((ref) => {
+    finalString += convertRef(ref);
+  });
+  return finalString;
+};
