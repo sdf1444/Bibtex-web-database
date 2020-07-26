a94d139ada37bbd8a208afb33d3a73e9e44cf7cd
Spencer Du <spencer.du@embl.de>
2020-07-27 00:45:31 +0200

    final

--- client/src/components/paper/PaperTableRow.js
+++ client/src/components/paper/PaperTableRow.js
@@ -6,7 +6,11 @@ const PaperTableRow = (props) => {
   const d = new Date(props.file.uploadDate);
   return (
     <tr>
-      <td>{props.file.filename}</td>
+      <td>
+        <a href={`http://localhost:5000/api/files/${props.file.filename}`}>
+          {props.file.filename}
+        </a>
+      </td>
       <td>{`${d.toLocaleDateString()} ${d.toLocaleTimeString()}`}</td>
       <td>{Math.round(props.file.length / 100) / 10 + 'KB'}</td>
       <td>
