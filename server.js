ff81406018da2b9d78e91f5e6cbeebf1e89aac6b
Spencer Du <spencer.du@embl.de>
2020-07-28 15:01:34 +0200

    finish

--- server.js
+++ server.js
@@ -21,10 +21,6 @@ app.use('/api/group', require('./routes/group'));
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static('client/build'));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
 }
 
 // error handler middleware
