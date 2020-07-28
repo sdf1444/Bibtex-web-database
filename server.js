58ca93ff80ba5f42b6d55f8ad1b801e19ba379c7
Spencer Du <spencer.du@embl.de>
2020-07-28 20:39:13 +0200

    final

--- server.js
+++ server.js
@@ -58,6 +58,10 @@ app.use(function (req, res, next) {
 if (process.env.NODE_ENV === 'production') {
   app.use('/public', express.static(path.join(__dirname, 'client/public')));
   app.use(express.static(path.join(__dirname, 'client/build')));
+
+  app.get('*', (request, response) => {
+    response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
 }
 
 app.use(bodyParser.json());
