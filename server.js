6353d25234ea9ff628a4f41a78459670cd61ccb0
Spencer Du <spencer.du@embl.de>
2020-07-28 05:21:23 +0200

    final

--- server.js
+++ server.js
@@ -42,6 +42,10 @@ const group = require('./routes/group');
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
 /** Seting up server to accept cross-origin browser requests */
