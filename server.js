9ae0ff0cf76b3a9f29c02fca0c0fe9715625a670
Spencer Du <spencer.du@embl.de>
2020-07-28 20:59:55 +0200

    final

--- server.js
+++ server.js
@@ -56,12 +56,9 @@ app.use(function (req, res, next) {
 
 // Serve static files from the React app
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use('/public', express.static(path.join(__dirname, 'client/public')));
+  app.use(express.static(path.join(__dirname, 'client/build')));
 }
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './client/build/index.html'));
-});
 
 app.use(bodyParser.json());
 app.use(logger('dev'));
