58705201ec9d97c9acb7c99c9701ebe7316cc508
Spencer Du <spencer.du@embl.de>
2020-07-29 02:29:16 +0200

    final

---
+++ client/build/precache-manifest.9169902560a0bbabc87b25c29f8c05ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "427c899944d6ec517f4a64995fa684ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "585f6dca40a491a7d04c",
+    "url": "/static/css/main.3c5902a7.chunk.css"
+  },
+  {
+    "revision": "54942d99b1341bfcb85f",
+    "url": "/static/js/2.1356237c.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.1356237c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "585f6dca40a491a7d04c",
+    "url": "/static/js/main.0f5ba3f9.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
