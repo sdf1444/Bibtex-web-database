185ac180dc443bf7077031c750494a6077e0e01c
Spencer Du <spencer@wearelighten.co.uk>
2023-01-14 05:39:08 +0000

    ad

--- client/src/actions/group.js
+++ client/src/actions/group.js
@@ -1,18 +1,18 @@
 import axios from 'axios';
 
 export const getUser = async () => {
-  const res = await axios.get('/api/user/me');
+  const res = await axios.get('https://bibtex-webdatabase.herokuapp.com/api/user/me');
   return res;
 };
 
 export const getGroups = async (type) => {
-  const res = await axios.get(`/api/group/${type}`);
+  const res = await axios.get(`https://bibtex-webdatabase.herokuapp.com/api/group/${type}`);
   return res;
 };
 
 export const deleteGroup = async (groupId) => {
   try {
-    const res = await axios.delete(`/api/group/${groupId}`);
+    const res = await axios.delete(`https://bibtex-webdatabase.herokuapp.com/api/group/${groupId}`);
     return res;
   } catch (err) {
     return err.response;
@@ -21,7 +21,7 @@ export const deleteGroup = async (groupId) => {
 
 export const leaveGroup = async (groupId) => {
   try {
-    const res = await axios.post(`/api/group/leave`, {
+    const res = await axios.post(`https://bibtex-webdatabase.herokuapp.com/api/group/leave`, {
       id: groupId,
     });
     return res;
@@ -32,7 +32,7 @@ export const leaveGroup = async (groupId) => {
 
 export const joinGroup = async (groupId) => {
   try {
-    const res = await axios.post('/api/group/join-request', {
+    const res = await axios.post('https://bibtex-webdatabase.herokuapp.com/api/group/join-request', {
       id: groupId,
     });
     return res;
@@ -43,7 +43,7 @@ export const joinGroup = async (groupId) => {
 
 export const createGroup = async (name) => {
   try {
-    const res = await axios.post('/api/group/', { name });
+    const res = await axios.post('https://bibtex-webdatabase.herokuapp.com/api/group/', { name });
     return res;
   } catch (err) {
     return err.response;
@@ -52,7 +52,7 @@ export const createGroup = async (name) => {
 
 export const saveGroup = async (name, id) => {
   try {
-    const res = await axios.put('/api/group', { name, id });
+    const res = await axios.put('https://bibtex-webdatabase.herokuapp.com/api/group', { name, id });
     return res;
   } catch (err) {
     return err.response;
@@ -61,7 +61,7 @@ export const saveGroup = async (name, id) => {
 
 export const acceptMember = async (groupId, userId) => {
   try {
-    const res = await axios.post('/api/group/accept', { groupId, userId });
+    const res = await axios.post('https://bibtex-webdatabase.herokuapp.com/api/group/accept', { groupId, userId });
     return res;
   } catch (err) {
     return err.response;
@@ -70,7 +70,7 @@ export const acceptMember = async (groupId, userId) => {
 
 export const rejectMember = async (groupId, userId) => {
   try {
-    const res = await axios.post('/api/group/decline', { groupId, userId });
+    const res = await axios.post('https://bibtex-webdatabase.herokuapp.com/api/group/decline', { groupId, userId });
     return res;
   } catch (err) {
     return err.response;
@@ -79,7 +79,7 @@ export const rejectMember = async (groupId, userId) => {
 
 export const banMember = async (groupId, userId) => {
   try {
-    const res = await axios.post('/api/group/ban', { groupId, userId });
+    const res = await axios.post('https://bibtex-webdatabase.herokuapp.com/api/group/ban', { groupId, userId });
     return res;
   } catch (err) {
     return err.response;
