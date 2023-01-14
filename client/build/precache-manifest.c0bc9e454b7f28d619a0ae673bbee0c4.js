3c47f89795416a5d92bcffe2af23fe2abe3da22a
Spencer Du <spencer@wearelighten.co.uk>
2023-01-14 04:55:49 +0000

    ad

---
+++ client/build/precache-manifest.c0bc9e454b7f28d619a0ae673bbee0c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8422dfce44cc1001917c10e296412dca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18737ca12cde218598f2",
+    "url": "/static/css/main.560ee835.chunk.css"
+  },
+  {
+    "revision": "7a72ace7700220d50e18",
+    "url": "/static/js/2.368eac00.chunk.js"
+  },
+  {
+    "revision": "a816be472df7ca8bcd8e946259be6542",
+    "url": "/static/js/2.368eac00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18737ca12cde218598f2",
+    "url": "/static/js/main.676918fa.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.676918fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05fbb5ed970a924e23b7",
+    "url": "/static/js/runtime-main.81534a39.js"
+  }
+]);
